43f6a24c6d594e4c4f96b7f03839054cdcc8629e
Alexander F. Mueller <>
2021-12-27 23:16:18 +0100

    Add first try for Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var result = 0
+        var prev = 99999
+        for (s in input) {
+            if (s > prev) { result += 1}
+            prev = s
+            }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +16,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
